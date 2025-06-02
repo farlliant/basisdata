529415582897d51080f89474b3a5fc51d70ab7bf
farrel <farrellbrilliantt@gmail.com>
2025-06-02 15:38:42 +0700

    basis data

---
+++ src/main/java/id/basisdata/Basisdata.java
@@ -0,0 +1,42 @@
+package id.basisdata;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.view.JasperViewer;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Basisdata {
+    private static String destFileName = "CLASSROOM.pdf";
+    public static void main(String[] args) throws FileNotFoundException, JRException {
+        System.out.println("generating jasper report...");
+        JasperReport jasperReport = getJasperReport();
+        Map<String, Object> parameters = getParameters();
+        String connectionUrl = 
+            "jdbc:sqlserver://localhost:1433;" +
+            "database=University; user=xyz; password=123;" +
+            "trustServerCertificate=true;" +
+            "loginTimeout=30;";
+        try {
+            Connection con = DriverManager.getConnection(connectionUrl);
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, con);
+            JasperViewer.viewReport(jasperPrint, false);
+            JasperExportManager.exportReportToPdfFile(jasperPrint, destFileName);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    private static JasperReport getJasperReport() throws FileNotFoundException, JRException {
+        File template = Paths.get("resources/Simple_Blue.jrxml").toFile();
+        return JasperCompileManager.compileReport(template.getAbsolutePath());
+    }
+    private static Map<String, Object> getParameters() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("createdBy", "Putra");
+        return parameters;
+    }
+}
